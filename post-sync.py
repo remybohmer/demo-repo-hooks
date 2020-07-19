568b15719259862e3cdd6c1c8fa070f959bd29ec
Remy Bohmer <github@bohmer.net>
2020-07-19 21:59:39 +0200

    Add initial hook for post-sync repo hook
    Signed-off-by: Remy Bohmer <github@bohmer.net>

---
+++ post-sync.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def main(project_list, worktree_list=None, **kwargs):
+    """Main function invoked directly by repo.
+    We must use the name "main" as that is what repo requires.
+    Args:
+      project_list: List of projects to run on.
+      worktree_list: A list of directories.  It should be the same length as
+          project_list, so that each entry in project_list matches with a
+          directory in worktree_list.  If None, we will attempt to calculate
+          the directories automatically.
+      kwargs: Leave this here for forward-compatibility.
+    """
+    for project in project_list:
+      print("Would run sync-hook for project '%s'" % project)
+
+    if worktree_list:
+      for worktree in worktree_list:
+        print("would run sync-hook for worktree '%s'" % worktree)
+    
+
